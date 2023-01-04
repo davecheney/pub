ea0307cbd0ff1d0a0d0f19143adbd52d4aed75f3
Dave Cheney <dave@cheney.net>
2023-01-04 18:55:22 +1100

    fix followcmd

--- follow.go
+++ follow.go
@@ -2,20 +2,15 @@ package main
 
 import (
 	"context"
-	"fmt"
-	"net/url"
-	"path"
 
-	"github.com/carlmjohnson/requests"
 	"github.com/davecheney/pub/internal/activitypub"
 	"github.com/davecheney/pub/internal/models"
-	"github.com/google/uuid"
 	"gorm.io/gorm"
 )
 
 type FollowCmd struct {
-	Object string `help:"object to follow"`
-	Actor  string `help:"actor to follow with"`
+	Object string `help:"object to follow" required:"true"`
+	Actor  string `help:"actor to follow with" required:"true"`
 }
 
 func (f *FollowCmd) Run(ctx *Context) error {
@@ -23,55 +18,16 @@ func (f *FollowCmd) Run(ctx *Context) error {
 	if err != nil {
 		return err
 	}
-	var instance models.Instance
-	if err := db.First(&instance).Error; err != nil {
-		return err
-	}
 
-	username, domain, err := parseActor(f.Actor)
-	if err != nil {
-		return err
-	}
-
-	account, err := findLocalAccount(db, username, domain)
-	if err != nil {
-		return err
-	}
-
-	var actor map[string]interface{}
-	if err := requests.URL(f.Actor).Accept(`application/ld+json; profile="https://www.w3.org/ns/activitystreams"`).ToJSON(&actor).Fetch(context.Background()); err != nil {
-		return err
-	}
-
-	u, err := url.Parse(f.Object)
-	if err != nil {
-		return err
-	}
-	c, err := activitypub.NewClient(context.Background(), account)
-	if err != nil {
-		return err
-	}
-	return c.Post(fmt.Sprintf("%s://%s/inbox", u.Scheme, u.Host), map[string]any{
-		"@context": "https://www.w3.org/ns/activitystreams",
-		"id":       fmt.Sprintf("https://%s/%s", instance.Domain, uuid.New().String()),
-		"type":     "Follow",
-		"object":   f.Object,
-		"actor":    f.Actor,
-	})
-}
-
-func findLocalAccount(db *gorm.DB, username, domain string) (*models.Account, error) {
 	var account models.Account
-	if err := db.Where("username = ? AND domain = ?", username, domain).Joins("LocalAccount").First(&account).Error; err != nil {
-		return nil, err
+	if err := db.Joins("Actor", &models.Actor{URI: f.Actor}).Take(&account).Error; err != nil {
+		return err
 	}
-	return &account, nil
-}
 
-func parseActor(acct string) (string, string, error) {
-	url, err := url.Parse(acct)
+	client, err := activitypub.NewClient(context.Background(), &account)
 	if err != nil {
-		return "", "", fmt.Errorf("splitAcct: %w", err)
+		return err
 	}
-	return path.Base(url.Path), url.Host, nil
+
+	return client.Follow(account.Actor.URI, f.Object)
 }
