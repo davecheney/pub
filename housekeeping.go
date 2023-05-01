8459a57d51602062e50a1469abd309ff7fac8e37
Dave Cheney <dave@cheney.net>
2023-05-01 20:49:44 +1000

    housekeeping: delete orphaned conversations

--- housekeeping.go
+++ housekeeping.go
@@ -41,6 +41,18 @@ func (c *HouseKeepingCmd) Run(ctx *Context) error {
 		}
 		fmt.Println("deleted", res.RowsAffected, "actors with no statuses")
 
+		// delete all conversations that have no statuses
+		res = tx.Exec(`
+			DELETE FROM conversations
+			WHERE id NOT IN (
+				SELECT conversation_id FROM statuses
+			)
+		`)
+		if res.Error != nil {
+			return res.Error
+		}
+		fmt.Println("deleted", res.RowsAffected, "orphaned conversations")
+
 		return nil
 	})
 }
